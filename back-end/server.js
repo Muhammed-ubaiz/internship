4f286ea9a86885a61a00358e8abe56576c97253f
ubais <muhammedubais01@gmail.com>
2025-12-31 10:34:04 +0530

    server

---
+++ back-end/server.js
@@ -0,0 +1,20 @@
+import express from "express"
+import dotenv from "dotenv"
+import cors from "cors"
+import connectDB from "./Database/Connetion.js";
+
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+app.use(cors())
+app.use(express.json());
+
+connectDB()
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
