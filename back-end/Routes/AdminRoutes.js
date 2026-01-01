9782ddb77bee22948b33c4209ba5c6f54de1019c
Navaneethkkkk <navaneethkk03@gmail.com>
2026-01-01 17:29:33 +0530

    error clear

--- back-end/Routes/AdminRoutes.js
+++ back-end/Routes/AdminRoutes.js
@@ -1,25 +1,19 @@
 import express from "express";
 
-import { Login } from "../Controller/AdminController.js";
+import { Login,addCourse,getCourse,deleteCourse, } from "../Controller/AdminController.js";
 import { verifyToken } from "../AuthMiddleware.js";
 
 const adminRoutes = express.Router();
 
 
-adminRoutes.post("/login", Login);
 
 
 
 
 
-import {
-  Login,
-  addCourse,
-  getCourse,
-  deleteCourse,
-} from "../Controller/AdminController.js";
 
-const adminRoutes = express.Router();
+
+
 
 /* AUTH */
 adminRoutes.post("/login", Login);
