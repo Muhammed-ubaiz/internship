ddcad322c6ac3829a8bee04e010d6ae731e92f8e
Navaneethkkkk <navaneethkk03@gmail.com>
2026-01-01 16:48:16 +0530

    jwtsetting

--- frond-end/src/Routes/LayoutRoutes.jsx
+++ frond-end/src/Routes/LayoutRoutes.jsx
@@ -1,23 +1,40 @@
-import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import AdminLogin from '../pages/adminpage/AdminLogin'
-import Studentsdashboard from '../pages/Studentsdashboard'
-import Admindashboard from '../pages/adminpage/Admindashboard'
-import SideBarStudent from '../pages/studentpage/SideBarStudent'
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import AdminLogin from "../pages/adminpage/AdminLogin";
+import Studentsdashboard from "../pages/Studentsdashboard";
+import Admindashboard from "../pages/adminpage/Admindashboard";
+import SideBarStudent from "../pages/studentpage/SideBarStudent";
+
+import ProtectedRoute from "../../ProtectedRoute";
 
 function LayoutRoutes() {
   return (
     <>
       <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<AdminLogin/>}/>
-        <Route path='/studentsdashboard' element={<Studentsdashboard/>}/>
-        <Route path='/admindashboard' element={<Admindashboard/>}/>
-        <Route path='/sidebarstudent' element={<SideBarStudent/>}/>
-      </Routes>
+        <Routes>
+          <Route
+            path="/adminlogin"
+            element={
+             
+                <AdminLogin />
+           
+            }
+          />
+          <Route
+            path="/admindashboard"
+            element={
+              <ProtectedRoute>
+                <Admindashboard />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="/studentsdashboard" element={<Studentsdashboard />} />
+
+          <Route path="/sidebarstudent" element={<SideBarStudent />} />
+        </Routes>
       </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default LayoutRoutes
+export default LayoutRoutes;
