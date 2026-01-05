89655e5303a03befa23db73add7bddfbc737523f
ubais <muhammedubais01@gmail.com>
2026-01-05 15:05:19 +0530

    batch add delete

---
+++ back-end/Model/Batchmodel.js
@@ -0,0 +1,8 @@
+import mongoose from "mongoose";
+
+const batchSchema = new mongoose.Schema({
+  courseName: { type: String, required: true }, // store name instead of ID
+  name: { type: String, required: true },       // batch name
+});
+
+export default mongoose.model("Batch", batchSchema);
--- back-end/Routes/AdminRoutes.js
+++ back-end/Routes/AdminRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 
-import { Login,addCourse,getCourse,deleteCourse, } from "../Controller/AdminController.js";
+import { Login,addCourse,getCourse,deleteCourse, getBatches, addBatch, updateCourse, deleteBatch, } from "../Controller/AdminController.js";
 import { verifyToken } from "../AuthMiddleware.js";
 
 const adminRoutes = express.Router();
@@ -12,6 +12,12 @@ adminRoutes.post("/login", Login);
 adminRoutes.post("/addCourse",verifyToken, addCourse);
 adminRoutes.get("/getCourse",verifyToken, getCourse);
 adminRoutes.delete("/deleteCourse/:id", deleteCourse);
+adminRoutes.put("/updateCourse/:courseId", updateCourse);
+
+
+adminRoutes.get("/getBatches/:courseName", getBatches);
+adminRoutes.post("/addBatch/:courseName", addBatch);
+adminRoutes.delete("/deleteBatch/:batchId", deleteBatch);
 
 
 export default adminRoutes;
